88a1367a5bdafa3bdcb6f04b1921f56ebf405a59
tammy vu <tammy.vsk@gmail.com>
2023-03-01 12:13:31 -0600

    tostring

--- Date.java
+++ Date.java
@@ -1,5 +1,44 @@
 public class Date {
+
+    private int month;
+    private int day;
+    private int year;
+
+    public Date(int theMonth, int theDay, int theYear){
+        month = theMonth;
+        day = theDay;
+        year = theYear;
+    }
+
+    public int getMonth(){
+        return month;
+    }
+
+    public int getDay(){
+        return day;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public String toString(){
+        String result = "";
+        if (month < 10){
+            result += "0" + month + "/";
+        }else{
+            result += month + "/";
+        }
+        if (day < 10){
+            result += "0" + day + "/";
+        }else{
+            result += day + "/";
+        }
+        result += year;
+        return result;
+    }
     public static void main(String[] args) {
-        
+        Date d1 = new Date(10, 01, 1998);
+        System.out.println();
     }
 }
